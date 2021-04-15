e4b3f0f96cfd64759b95ac45f8eaa57257d8202c
juliakispert <54856485+juliakispert@users.noreply.github.com>
2021-04-14 20:33:54 -0500

    error with images fixed

--- components/UserMap.js
+++ components/UserMap.js
@@ -62,7 +62,7 @@ function userMap(props) {
                             longitude: -93.168869019}}
                             title={'CC Cache'}
                             key={Marker.snellAndGrand}
-                            image={require("./Geocache/assets/scot.png")}
+                            image={require("./scot.png")}
                           />
               </MapView>
               <View style={[styles.bubble, styles.latlng]}>
