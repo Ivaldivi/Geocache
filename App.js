e79b849c13b69222a80d80a580b0d5fc65006345
juliakispert <54856485+juliakispert@users.noreply.github.com>
2021-04-20 13:48:44 -0500

    About Page now in component form to clean up app.js

--- App.js
+++ App.js
@@ -9,8 +9,10 @@ import Compass from './components/Compass';
 import { Component } from 'react';
 import UserMap2 from './components/UserMap2';
 import Victory from './components/Victory';
+import About from './components/About';
 import MapView, {Marker} from 'react-native-maps';
 import { apisAreAvailable } from 'expo';
+import { Grid, Col, Row } from "react-native-easy-grid";
 
 
 const { width, height } = Dimensions.get('window');
@@ -183,39 +185,13 @@ const WelcomeScreen = ({ navigation }) => {
     </View>
   )
 }
-const B = (props) => <Text style={{fontWeight: 'bold', fontSize: 20}}>{props.children}</Text>
-//https://stackoverflow.com/questions/35718143/react-native-add-bold-or-italics-to-single-words-in-text-field#:~:text=You%20can%20use%20like,...
+
 
 const AboutPage = ({ navigation }) => {
   return (
-    <ScrollView style = {{backgroundColor: 'rgba(223, 108, 22, 1)', height: Dimensions.get('window').height}}>
-    <Text style = {{ fontSize: 25, fontWeight: 'bold', marginLeft: 4, marginBottom: 5, borderColor: 'white',borderWidth: 2, borderRadius: 8,
-      color:'#183592',marginRight: 285}}>
-        Rules:  </Text>
-    <Text style = {styles.AboutPageP}>
-      <B>1.</B> Be nice in the message log! No mean-spirited, hateful, or discriminatory messages. 
-      {'\n'}{'\n'}<B>2.</B> Be careful when using the map! Make sure to look up from your phone and stay aware of your surroundings
-       </Text>
-    <Text style = {styles.AboutPageP}>
-      {'\n'}<B>3.</B> Obey traffic safety laws when using the compass.
-      Our compass feature points you directly to the goal. The compass does not take into account roads, private property, or other hazards.{'\n'}
-      </Text>
-    <Text style = {{ fontSize: 25, fontWeight: 'bold', marginLeft: 4, marginBottom: 5, borderColor: 'white',borderWidth: 2, borderRadius: 8, color:'#183592',marginRight: 195}}>
-      Who Are We?
-      </Text>
-    <Text style = {styles.AboutPageP}>
-      #izadjaju-- This app was created as a software development project for the Macalester College class COMP225 Software Development.
-      The four main authors of the project are: Julia Kispert, A'di Dust, James Bellitto, and Izzy Valdivia with lots of help from our instructor! {'\n'} 
-      </Text>
-    <Text style = {{ fontSize: 25, fontWeight: 'bold', marginLeft: 4, marginBottom: 5, borderColor: 'white',borderWidth: 2, borderRadius: 8, color:'#183592',marginRight: 105}}>
-      Acknowledgements:
-      </Text>
-    <Text style = {styles.AboutPageP}>
-      - React Native documentation (woohoo, we love you, thank you) {'\n'}- Our instructor: Paul Cantrell 
-      {'\n'}- Some of our other commonly used resources below : P  
-      </Text>
-  </ScrollView>
-
+ <View>
+    <About/>
+  </View>
   )
 }
 
