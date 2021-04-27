e55c2d0b97aa2364ae007d136d7bc257e4642316
ruler <adust@macalester.edu>
2021-04-27 13:14:31 -0500

    Create Victory test file

--- _tests_/victory.test.js
+++ _tests_/victory.test.js
@@ -2,12 +2,6 @@ import React from 'react';
 import TestRenderer from 'react-test-renderer';
 import Victory from '../components/Victory';
 
-describe("<Victory />", () => {
-    it('has 1 child', () => {
-        const tree = TestRenderer.create(<Victory location={{ latitude: 44.934412433560745, longitude: -93.1777188451171 }}/>).toJSON();
-        expect(tree.children.length).toBe(1);
-    });
-});
 
 test('renders correctly', () => {
     const tree = TestRenderer.create(<Victory location={{ latitude: 44.934412433560745, longitude: -93.1777188451171 }}/>).toJSON();
