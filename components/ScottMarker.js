f7518790e18c7b89e3d9d85107d63e8755be4412
juliakispert <54856485+juliakispert@users.noreply.github.com>
2021-04-27 13:53:52 -0500

    possible image fix....

--- components/ScottMarker.js
+++ components/ScottMarker.js
@@ -8,8 +8,18 @@ const ScottMarker = props =>{
             coordinate={props.coordinates}
             title={props.title}
             key={Marker.snellAndGrand}
-            image={require("./images/scot.png")}
-            // onPress={() =>createNewMap(this)}
+            //image={require("./images/scot.png")}
+           // image = {require('scot.png')}
+           image={Platform.OS === 'android' ? require('scot.png')} : undefined}>
+            {Platform.OS === 'ios'
+  ? <Image
+      source={require('scot.png'}
+      style={{
+        width: 15,
+        height: 15,
+      }}
+    />
+  : null}
             onPress={()=> console.log(props.coordinates)}
           />
     )
