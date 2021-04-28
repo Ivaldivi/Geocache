954631d761eb74172f4b65c068a764297b764f7f
ruler <adust@macalester.edu>
2021-04-28 12:33:29 -0500

    Start on compass math tests

--- _tests_/victory.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import TestRenderer from 'react-test-renderer';
-import Victory from '../components/Victory';
-
-
-test('renders correctly', () => {
-    const tree = TestRenderer.create(<Victory location={{ latitude: 44.934412433560745, longitude: -93.1777188451171 }}/>).toJSON();
-    expect(tree).toMatchSnapshot();
-  });
